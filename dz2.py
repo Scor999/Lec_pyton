885c261bffd5da4dc22e7695e80e267c48ea4303
Scor999 <Scor999@yandex.ru>
2022-09-30 21:57:29 +0700

    решение 4  задачи

--- dz2.py
+++ dz2.py
@@ -28,7 +28,40 @@
 # Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 # Пример:- Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-n = int(input('Введите число: ')) 
-def sequence(n):
-    return[round((1 + 1 / x)**x) for x in range (1, n + 1)]   
-print("Для n=" , n,sequence(n),"->", round(sum(sequence(n))))
+# n = int(input('Введите число: ')) 
+# def sequence(n):
+#     return[round((1 + 1 / x)**x) for x in range (1, n + 1)]   
+# print("Для n=" , n,sequence(n),"->", round(sum(sequence(n))))
+
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+# from random import randint
+
+# with open('file.txt', 'w') as data:
+#     data.write('0\n')
+#     data.write('9\n')
+
+# def get_numbers(n):
+#     return [randint(-n/2, n) for i in range(-n, n + 1)]
+
+# def get_data_from_file(path):
+#     data = open(path, 'r')
+#     dlist = [int(line.strip()) for line in data]
+#     data.close()
+#     return dlist
+
+# def get_mult(numbers, datalist):
+#     mult = 1
+#     for i in datalist:
+#         mult *= numbers[i]
+#     return mult
+
+# path = 'file.txt'
+# n = 10 
+# datalist = get_data_from_file(path)
+# numbers = get_numbers(n)
+
+# print(numbers)
+# print(datalist)
+# print(get_mult(numbers, datalist))
