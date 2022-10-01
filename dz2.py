2862d7fd87686af6daf1033baadcde01b0174031
Scor999 <Scor999@yandex.ru>
2022-10-01 18:09:56 +0700

    добавление второго решения

--- dz2.py
+++ dz2.py
@@ -74,3 +74,23 @@
 # import random
 # random.shuffle(lst)
 # print('->', lst)
+
+#второе решение!!!
+
+import random
+def mix_list(list_original):
+    list = list_original[:]
+    list_length = len(list)
+    for i in range(list_length):
+        index_aleatory = random.randint(0, list_length - 1)
+        temp = list[i]
+        list[i] = list[index_aleatory]
+        list[index_aleatory] = temp
+        return list
+list= input('Введите список: ')
+list=list.split(" ")
+mixed_list = mix_list(list)
+print("Исходный список: ")
+print(list)
+print("Перемешанный список: ")
+print(mixed_list)
