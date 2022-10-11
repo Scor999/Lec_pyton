2bfbe290d06cca4eb4920d2dc3b7927ffa8d3e80
Scor999 <Scor999@yandex.ru>
2022-10-11 23:34:07 +0700

    ДЗ 5 крестики-нолики

--- dz5.py
+++ dz5.py
@@ -23,46 +23,123 @@
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
-from random import randint
+# from random import randint
 
-def input_dat(name):
-    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
-    while x < 1 or x > 28:
-        x = int(input(f"{name}, введите корректное количество конфет не жадничай))): "))
-    return x
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет не жадничай))): "))
+#     return x
 
 
-def p_print(name, k, counter, value):
-    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
 
-player1 = input("Введите имя первого игрока: ")
-player2 = input("Введите имя второго игрока: ")
-value = int(input("Введите количество конфет на столе: "))
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# value = int(input("Введите количество конфет на столе: "))
+# flag = randint(0,2) # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
 
-counter1 = 0 
-counter2 = 0
+# counter1 = 0 
+# counter2 = 0
 
-while value > 28:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
-    else:
-        k = input_dat(player2)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = input_dat(player2)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
 
-if flag:
-    print(f"Выиграл {player1}")
-else:
-    print(f"Выиграл {player2}")
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
 
+# Создайте программу для игры в "Крестики-нолики".
+
+# # Инициализация карты
+# maps = [1,2,3,
+#         4,5,6,
+#         7,8,9]
+ 
+# # Инициализация победных линий
+# victories = [[0,1,2],
+#              [3,4,5],
+#              [6,7,8],
+#              [0,3,6],
+#              [1,4,7],
+#              [2,5,8],
+#              [0,4,8],
+#              [2,4,6]]
+ 
+# # Вывод карты на экран
+# def print_maps():
+#     print(maps[0], end = " ")
+#     print(maps[1], end = " ")
+#     print(maps[2])
+ 
+#     print(maps[3], end = " ")
+#     print(maps[4], end = " ")
+#     print(maps[5])
+ 
+#     print(maps[6], end = " ")
+#     print(maps[7], end = " ")
+#     print(maps[8])    
+ 
+# # Сделать ход в ячейку
+# def step_maps(step,symbol):
+#     ind = maps.index(step)
+#     maps[ind] = symbol
+ 
+# # Получить текущий результат игры
+# def get_result():
+#     win = ""
+ 
+#     for i in victories:
+#         if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+#             win = "X"
+#         if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+#             win = "O"   
+             
+#     return win
+ 
+# # Основная программа
+# game_over = False
+# player1 = True
+ 
+# while game_over == False:
+ 
+#     # 1. Показываем карту
+#     print_maps()
+ 
+#     # 2. Спросим у играющего куда делать ход
+#     if player1 == True:
+#         symbol = "X"
+#         step = int(input("Человек 1, ваш ход: "))
+#     else:
+#         symbol = "O"
+#         step = int(input("Человек 2, ваш ход: "))
+ 
+#     step_maps(step,symbol) # делаем ход в указанную ячейку
+#     win = get_result() # определим победителя
+#     if win != "":
+#         game_over = True
+#     else:
+#         game_over = False
+ 
+#     player1 = not(player1)        
+ 
+# # Игра окончена. Покажем карту. Объявим победителя.        
+# print_maps()
+# print("Победил", win)
